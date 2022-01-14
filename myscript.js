56bc9a1de90c8043ac90255bd78173b6d4810f9c
BenedettiGianluca <99gian.bene@gmail.com>
2022-01-14 19:12:19 +0100

    Vue Slider

--- myscript.js
+++ myscript.js
@@ -1,124 +1,98 @@
-const items = [
-    'img/01.jpg',
-    'img/02.jpg',
-    'img/03.jpg',
-    'img/04.jpg',
-    'img/05.jpg'
-];
-
-const title = [
-    'Svezia',
-    'Svizzera',
-    'Gran Bretagna',
-    'Germania',
-    'Paradise'
-];
-
-const text = [
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
-    'Lorem ipsum',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
-    'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-    'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
-];
-
-let frecciaSopra = document.querySelector('.frecciaSopra');
-let frecciaSotto = document.querySelector('.frecciaSotto');
-let immagineSelezionata = 2;
 let trasparenza = document.querySelectorAll('.trasparenza');
-let immagineGrande = document.querySelector('.immagineGrande');
-let locazione = document.querySelector('.location');
-let descrizioneLocation = document.querySelector('.descrizioneLocation');
-
 trasparenza[1].style.backgroundColor='rgba(0, 0, 0, 0.0)';
 trasparenza[1].style.borderWidth='5px';
 trasparenza[1].style.borderStyle='inset';
 trasparenza[1].style.borderColor='#cccccc';
-immagineGrande.style.backgroundImage='url("consegna/img/02.jpg")';
-locazione.innerHTML=title[1];
-descrizioneLocation.innerHTML=text[1];
 
-frecciaSopra.addEventListener('click', function(){
-    if (immagineSelezionata>0){
-        immagineSelezionata = immagineSelezionata-1;
-    }
-    if (immagineSelezionata==0){
-        immagineSelezionata = 5;
-    }
-    for(x=0;x<5;x++){
-        trasparenza[x].style.backgroundColor='rgba(0, 0, 0, 0.6)';
-        trasparenza[x].style.border='none';
-    }
-    for (x=1;x<=5;x++){
-        if (immagineSelezionata==x){
-            trasparenza[x-1].style.backgroundColor='rgba(0, 0, 0, 0.0)';
-            trasparenza[x-1].style.borderWidth='5px';
-            trasparenza[x-1].style.borderStyle='inset';
-            trasparenza[x-1].style.borderColor='#cccccc';
-            switch(x) {
-                case 1:
-                    immagineGrande.style.backgroundImage='url("consegna/img/01.jpg")';
-                    break;
+var V = new Vue({
+    el: '#VueJS',
+    data: {
+        items: [
+            'img/01.jpg',
+            'img/02.jpg',
+            'img/03.jpg',
+            'img/04.jpg',
+            'img/05.jpg'
+        ],
+        
+        title: [
+            'Svezia',
+            'Svizzera',
+            'Gran Bretagna',
+            'Germania',
+            'Paradise'
+        ],
+        
+        text: [
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam, cumque provident totam omnis, magnam dolores dolorum corporis.',
+            'Lorem ipsum',
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit.',
+            'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
+            'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,'
+        ],
 
-                case 2:
-                    immagineGrande.style.backgroundImage='url("consegna/img/02.jpg")';
-                    break;
-
-                case 3:
-                    immagineGrande.style.backgroundImage='url("consegna/img/03.jpg")';
-                    break;
-
-                case 4:
-                    immagineGrande.style.backgroundImage='url("consegna/img/04.jpg")';
-                    break;
-            
-                default: immagineGrande.style.backgroundImage='url("consegna/img/05.jpg")';
-                    break;
+        immagineSelezionata: 2,
+        locazione: 'Svizzera',
+        descrizioneLocation: 'Lorem ipsum'
+    },
+    methods: {
+        clickFreccia: function(){
+            let trasparenza = document.querySelectorAll('.trasparenza');
+            let immagineGrande = document.querySelector('.immagineGrande');
+            for (x=0;x<5;x++){
+                trasparenza[x].style.backgroundColor='rgba(0, 0, 0, 0.6)';
+                trasparenza[x].style.border='none';
             }
-            locazione.innerHTML=title[x-1];
-            descrizioneLocation.innerHTML=text[x-1];
-        }
-    }
-});
-frecciaSotto.addEventListener('click', function(){
-    if (immagineSelezionata<6){
-        immagineSelezionata = immagineSelezionata+1;
-    }
-    if (immagineSelezionata==6){
-        immagineSelezionata = 1;
-    }
-    for(x=0;x<5;x++){
-        trasparenza[x].style.backgroundColor='rgba(0, 0, 0, 0.6)';
-        trasparenza[x].style.border='none';
-    }
-    for (x=1;x<=5;x++){
-        if (immagineSelezionata==x){
-            trasparenza[x-1].style.backgroundColor='rgba(0, 0, 0, 0.0)';
-            trasparenza[x-1].style.borderWidth='5px';
-            trasparenza[x-1].style.borderStyle='inset';
-            trasparenza[x-1].style.borderColor='#cccccc';
-            switch(x) {
-                case 1:
-                    immagineGrande.style.backgroundImage='url("consegna/img/01.jpg")';
-                    break;
-
-                case 2:
-                    immagineGrande.style.backgroundImage='url("consegna/img/02.jpg")';
-                    break;
-
-                case 3:
-                    immagineGrande.style.backgroundImage='url("consegna/img/03.jpg")';
-                    break;
-
-                case 4:
-                    immagineGrande.style.backgroundImage='url("consegna/img/04.jpg")';
-                    break;
-            
-                default: immagineGrande.style.backgroundImage='url("consegna/img/05.jpg")';
-                    break;
+            for (x=1;x<=5;x++){
+                if (V.immagineSelezionata==x){
+                    trasparenza[x-1].style.backgroundColor='rgba(0, 0, 0, 0.0)';
+                    trasparenza[x-1].style.borderWidth='5px';
+                    trasparenza[x-1].style.borderStyle='inset';
+                    trasparenza[x-1].style.borderColor='#cccccc';
+                    switch(x) {
+                        case 1:
+                            immagineGrande.style.backgroundImage='url("consegna/img/01.jpg")';
+                            break;
+        
+                        case 2:
+                            immagineGrande.style.backgroundImage='url("consegna/img/02.jpg")';
+                            break;
+        
+                        case 3:
+                            immagineGrande.style.backgroundImage='url("consegna/img/03.jpg")';
+                            break;
+        
+                        case 4:
+                            immagineGrande.style.backgroundImage='url("consegna/img/04.jpg")';
+                            break;
+                    
+                        default: immagineGrande.style.backgroundImage='url("consegna/img/05.jpg")';
+                            break;
+                    }
+                    V.locazione=V.title[x-1];
+                    V.descrizioneLocation=V.text[x-1];
+                }
             }
-            locazione.innerHTML=title[x-1];
-            descrizioneLocation.innerHTML=text[x-1];
+        },
+
+        clickFrecciaSopra: function(){
+            if (V.immagineSelezionata>0){
+                V.immagineSelezionata = V.immagineSelezionata-1;
+            }
+            if (V.immagineSelezionata==0){
+                V.immagineSelezionata = 5;
+            }
+            V.clickFreccia();
+        },
+
+        clickFrecciaSotto: function(){
+            if (V.immagineSelezionata<6){
+                V.immagineSelezionata = V.immagineSelezionata+1;
+            }
+            if (V.immagineSelezionata==6){
+                V.immagineSelezionata = 1;
+            }
+            V.clickFreccia();
         }
     }
 });
